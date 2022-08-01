dbb2038a0e969b381761bb64eefeb4f85d7beb16
liuxiaokun <liuxiaokun@shengchaotec.com>
2022-08-01 14:47:27 +0800

    refine config

--- src/main/java/com/example/eshighlevelapi/config/RestElasticSearchClientConfig.java
+++ src/main/java/com/example/eshighlevelapi/config/RestElasticSearchClientConfig.java
@@ -3,17 +3,22 @@ package com.example.eshighlevelapi.config;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RestElasticSearchClientConfig {
-    // 将方法的返回结果交给spring管理
+
+    @Value("${elasticsearch.hostname}")
+    private String hostname;
+
+    /**
+     * 将方法的返回结果交给spring管理
+     */
     @Bean
-    public RestHighLevelClient restHighLevelClient(){
+    public RestHighLevelClient restHighLevelClient() {
         // 主机ip和端口号以及协议
-        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(RestClient.builder(
-                new HttpHost("172.20.243.7", 9200, "http")));
-        return restHighLevelClient;
+        return new RestHighLevelClient(RestClient.builder(new HttpHost(hostname, 9200, "http")));
     }
 }
